1faa79807f00b68f8d4019707887e18b686e67d5
Mark Akom <mark.akom2019@gmail.com>
2021-11-02 08:54:38 +0000

    created the code for the computer selection

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log('working');
+function computerPlay() {
+    let computerSelection;
+    let randomNumber = (Math.floor(Math.random()* 3) + 1 )
+
+    if (randomNumber === 1) {
+        computerSelection = 'rock';
+    } else if (randomNumber === 2) {
+        computerSelection = 'paper';
+    } else {
+        computerSelection = 'scissors';
+    }
+
+    return computerSelection;
+}
+
+console.log(computerPlay());
