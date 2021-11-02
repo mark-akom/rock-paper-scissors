c8bf29600d36eeebe28b44b234deb5b042c3b61f
Mark Akom <mark.akom2019@gmail.com>
2021-11-02 10:06:17 +0000

    added code for the playRound function to determine who wins a round

--- js/index.js
+++ js/index.js
@@ -13,4 +13,41 @@ function computerPlay() {
     return computerSelection;
 }
 
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let message;
+    //get the outcome of the game
+    if(playerSelection === computerSelection) {
+        message = "It's a tie";
+        return message;
+    }
+
+    if (playerSelection === 'rock') {
+        if(computerSelection === 'scissors') {
+            message = 'You win, Rock beats Scissors';
+        } else {
+            message = 'You lose, Paper beats Rock';
+        }
+        return message;
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            message = 'You win, Paper beats Rock';
+        } else {
+            message = 'You lose, Scissors beats Paper';
+        }
+        return message;
+    } else {
+        if(computerSelection === 'paper') {
+            message = 'You win, Scissors beats Paper';
+        } else {
+            message = 'You lose, Rock beats Scissors'
+        }
+        return message;
+    }
+}
+
+const playerSelection = 'paper';
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
