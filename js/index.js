ceb1967734c0542ce899e784139f398b60a9aefe
Mark Akom <mark.akom2019@gmail.com>
2021-11-02 10:43:13 +0000

    Added variables playerscore and computerscore to keep track of score. edited the playRound to score winner of each round. Added a new function game with plays the round 5 times and outputs the winner of the game

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     let computerSelection;
     let randomNumber = (Math.floor(Math.random()* 3) + 1 )
@@ -26,28 +29,51 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock') {
         if(computerSelection === 'scissors') {
             message = 'You win, Rock beats Scissors';
+            playerScore += 1;
         } else {
             message = 'You lose, Paper beats Rock';
+            computerScore += 1;
         }
         return message;
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             message = 'You win, Paper beats Rock';
+            playerScore += 1;
         } else {
             message = 'You lose, Scissors beats Paper';
+            computerScore += 1;
         }
         return message;
     } else {
         if(computerSelection === 'paper') {
             message = 'You win, Scissors beats Paper';
+            playerScore += 1;
         } else {
             message = 'You lose, Rock beats Scissors'
+            computerScore += 1;
         }
         return message;
     }
 }
 
-const playerSelection = 'paper';
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    alert('Hi, there let\'s play rock paper scissors game')
+
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = prompt('What is your selection?');
+        let computerSelection = computerPlay();
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore > computerScore) {
+        console.log('Player wins the game')
+    } else if (computerScore > playerScore) {
+        console.log('Computer wins the game');
+    } else {
+        console.log('This game was a draw');
+    }
+}
+
+game();
