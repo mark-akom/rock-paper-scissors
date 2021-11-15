6c9678c76e55e74f1e98314f8421d4b1529484c8
Mark Akom <mark.akom2019@gmail.com>
2021-11-15 19:32:50 +0000

    Modified the html file and also completed js code for the new ui feature in the js file.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const playerSelectionButtons = document.querySelectorAll('button.player-selection');
+const gameOutcome = document.querySelector('div.outcome');
+const playerScoreDisplay = document.querySelector('p.player-score');
+const computerScoreDisplay = document.querySelector('p.cpu-score');
+const gameRuns = document.querySelector('p.game-runs-number');
+
 function computerPlay() {
     let computerSelection;
     let randomNumber = (Math.floor(Math.random()* 3) + 1 )
@@ -56,20 +62,33 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function findWinner() {
-    if (playerScore > computerScore) {
-        console.log(`Player wins the game. Score: ${playerScore} : ${computerScore}`)
-    } else if (computerScore > playerScore) {
-        console.log(`Computer wins the game. Score: ${computerScore} : ${playerScore}`)
-    } else {
-        console.log(`This game was a draw. Score: ${playerScore} : ${computerScore}`);
-    }
+
+const resetAll = () => {
+    
+    let roundWinner = playerScore === 5 ? 'Player' : 'Computer';
+    gameOutcome.textContent = `${roundWinner} wins this round. Play another game. Score: Player - (${playerScore}) Computer - (${computerScore})`;
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = 0;
+    computerScoreDisplay.textContent = 0;
+    gameRuns.textContent = 0;
 }
 
-
-function game() {
-    
-    
-    findWinner();
+const writeToUi = (whoWins) => {
+    gameOutcome.textContent = whoWins;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
 }
 
+playerSelectionButtons.forEach(playerSelection => {
+    playerSelection.addEventListener('click', (e) => {
+        gameRuns.textContent = Number(gameRuns.textContent) + 1;
+        const button = e.target;
+        let playerChoice = button.textContent;
+        let whoWins = playRound(playerChoice, computerPlay());
+        writeToUi(whoWins);
+        if (playerScore === 5 || computerScore === 5) {
+            resetAll();
+        }
+    })
+});
